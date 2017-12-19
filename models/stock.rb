2b0ff8f5852c526711bc0ab347b5194c34ea88f9
Joe Plevin <joe.plevin@hotmail.co.uk>
2017-12-19 09:35:24 +0000

    stock crud actions added

--- models/stock.rb
+++ models/stock.rb
@@ -13,4 +13,57 @@ class Stock
     @song_quantity = options ['song_id'].to_i
     @song_price = options['song_price'].to_f
   end
+
+  def save
+    sql = "INSERT INTO record_shop
+          (
+          album_id,
+          album_quantity,
+          album_price,
+          artist_id,
+          song_id,
+          song_quantity,
+          song_price
+          )
+          VALUES ($1,$2,$3,$4,$5,$6,$7)
+          RETURNING id"
+    values = [
+          @album_id,
+          @album_quantity,
+          @album_price,
+          @artist_id,
+          @song_id,
+          @song_quantity,
+          @song_price
+          ]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM stocks"
+    values = []
+    results = SqlRunner.run(sql, values)
+    return results.map{|stock| Stock.new(stock)}
+  end
+
+  def find
+    sql = "SELECT * WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Stock.new(results[0])
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM stocks"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def destory(id)
+    sql = "DELETE FROM stocks WHERE id = $1"
+    values = [id]
+    SqlRunner.run(sql, values)
+  end
+
 end
