72d3a9a73a1e085cb0be0b1b87efe5522bfa3507
Joe Plevin <joe.plevin@hotmail.co.uk>
2017-12-19 09:34:39 +0000

    song crud actions added

--- models/song.rb
+++ models/song.rb
@@ -10,5 +10,53 @@ class Song
     @length = options['length'].to_i
     @genre = options['genre']
   end
+  
+  def save
+    sql = "INSERT INTO record_shop
+          (
+          title,
+          artist_id,
+          album_id,
+          length,
+          genre
+          )
+          VALUES ($1,$2,$3,$4,$5)
+          RETURNING id"
+    values = [
+          @title,
+          @artist_id,
+          @album_id,
+          @length,
+          @genre
+          ]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM songs"
+    values = []
+    results = SqlRunner.run(sql, values)
+    return results.map{|song| Song.new(song)}
+  end
+
+  def self.find
+    sql = "SELECT * WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Song.new(results[0])
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM songs"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def destory(id)
+    sql = "DELETE FROM songs WHERE id = $1"
+    values = [id]
+    SqlRunner.run(sql, values)
+  end
 
 end
